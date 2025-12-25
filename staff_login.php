19eb45a9ed66ba0fbf37cb25774faf87901931e4
Mohammad Yasir <md.yasir.xt@gmail.com>
2025-12-25 13:28:28 +0600

    login logo updated

--- staff_login.php
+++ staff_login.php
@@ -110,6 +110,14 @@ include 'includes/header.php';
 
             <div class="card shadow-sm">
                 <div class="card-body">
+                    <div class="text-center mb-4">
+                        <div class="d-inline-flex align-items-center justify-content-center bg-danger bg-opacity-10 rounded-circle" style="width: 80px; height: 80px;">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="#dc3545" class="bi bi-person-badge" viewBox="0 0 16 16">
+                                <path d="M6.5 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zM11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                                <path d="M4.5 0A2.5 2.5 0 0 0 2 2.5V14a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2.5A2.5 2.5 0 0 0 11.5 0h-7zM3 2.5A1.5 1.5 0 0 1 4.5 1h7A1.5 1.5 0 0 1 13 2.5v10.795a4.2 4.2 0 0 0-.776-.492C11.392 12.387 10.063 12 8 12s-3.392.387-4.224.803a4.2 4.2 0 0 0-.776.492V2.5z"/>
+                            </svg>
+                        </div>
+                    </div>
                     <h3 class="card-title mb-4 text-center">Staff Login</h3>
                     <form method="post" action="">
                         <div class="mb-3">
